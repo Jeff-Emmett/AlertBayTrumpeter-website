ddb673ede67e7cce6b5724f29295b7fa8a1d3ffe
Jeff Emmett <jeffemmett@gmail.com>
2025-11-30 22:18:35 -0800

    Add 🎺 emoji favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -21,6 +21,9 @@ export const metadata: Metadata = {
   description:
     "The official website of Jerry Higginson, the Alert Bay Trumpeter, entertaining cruise ship passengers since 1996",
   generator: "v0.app",
+  icons: {
+    icon: "data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>🎺</text></svg>",
+  },
 }
 
 export default function RootLayout({
